af2f55e0e694f1bf0876c8e25aa4ffa95cb2478d
ManutiElArtista <guillermomjn@gmail.com>
2021-10-08 19:58:11 +0200

    Commit final
    Objetos aleatorios con Y cambiada en el tag Piramide.

--- PR_ZAXXON_JimenezGuillermo/Assets/Otros Scripts/Instanciador y columnas/InstanciadorColumna.cs
+++ PR_ZAXXON_JimenezGuillermo/Assets/Otros Scripts/Instanciador y columnas/InstanciadorColumna.cs
@@ -7,8 +7,10 @@ public class InstanciadorColumna : MonoBehaviour
 
     float intervalo;
 
-    [SerializeField] GameObject columna; // Le decimos que vamos a pasarle un objeto a este objeto que lo llamamos columna.
+    // [SerializeField] GameObject columna; // Le decimos que vamos a pasarle un objeto a este objeto que lo llamamos columna.
     [SerializeField] Transform instantiatePosition; // Le decimos que salgan desde la posicion que tenga el objeto que le pasemos.
+
+    [SerializeField] GameObject[] obstaculos; // Creamos un objeto de tipo array para llamar diferentes objetos.
 
     // Start is called before the first frame update
     void Start()
@@ -28,12 +30,23 @@ public class InstanciadorColumna : MonoBehaviour
     {
         while (true)
         {
-            float randomX = Random.Range(-14f, 14f);
-            Vector3 newPos = new Vector3(randomX, instantiatePosition.position.y, instantiatePosition.position.z);
-            Instantiate(columna, newPos, Quaternion.identity);
+            float randomX = Random.Range(-14f, 14f); // Le damos un valor aleatorio para que salga entre dos posiciones, minima y maxima.
+            float randomY = Random.Range(0.05f, 5f); // Le damos un valor aleatorio para que salga entre dos posiciones, minima y maxima.
 
+            Vector3 newPos = new Vector3(randomX, instantiatePosition.position.y, instantiatePosition.position.z); // Le decimos que random es un nuevo vector 3 que es el valo
+            Vector3 newPosY = new Vector3(randomX, randomY, instantiatePosition.position.z); // Le decimos que el random Y y el randomX sea una nueva posicion.
+
+            int numAl = Random.Range(0, obstaculos.Length); // Creamos una variable que diga que sea un numero aleatorio entre el valor 0 (1) y el valor maximo va a ser el numero maximo de objetos que le hayamos instanciado.
+
+            if (obstaculos[numAl].tag == "Piramide") // Le decimos que si el tag del gameObject sea Piramide, que salga aleatorio en posicion X e Y.
+            {
+                Instantiate(obstaculos[numAl], newPosY, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(obstaculos[numAl], newPos, Quaternion.identity); // Instanciamos al objeto que tenga el script el objeto aleatorio que haya salido anteriormente, con la posicion creada anteriormente y misma rotacion.
+            }
             yield return new WaitForSeconds(intervalo);
         }
     }
 }
-
